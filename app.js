212a59d23d8ac203dab598d465e213edf3034452
Juan Sifontez <juansfontz@gmail.com>
2021-11-30 21:49:47 -0400

    Lesson 7: Add ejs view engine
    Install ejs package
    Delete all html files
    Add ejs files to show the views
    In app.js:
      Config the view engine
      Change every get response to use the views

--- app.js
+++ app.js
@@ -6,44 +6,43 @@ const app = express();
 // listen for request. Same way that we do with 'http.createServer'
 app.listen(3000); // this also return an instance of the created server
 
+// register view engines. For inject dynamic data to the views
+// for that we use 'set' method which take two arguments.
+// 1st The name of the property
+// 2nd the value
+app.set('view engine', 'ejs');
 /**
- * Now you can listen all the request like GET, POST, DELETE and so on
- * for that we use the 'get' method of express. Which take 2 arguments.
- * the first one is the directory that we will be listening and the second
- * a callback function with request and response parameters
+ * By default 'ejs' will search for the templates in 'views' directory
+ * If we want to use another directory, we need to configure that directory
+ * using 'set' method again
+ */
+// app.set('views', 'myviews');
+
+/**
+ * here instead of using a file with 'sendFile' method.
+ * We now will goin to render a view.
  */
 app.get('/', (req, res) => {
-  // here we can set the Content-Type implicit usign 'send' method of response object
-  // this method also set the status code
-  // res.send('<p>Home page</p>');
-
-  /**
-   * for use directory to a response, we use the 'sendFile' method
-   * Which we pass the path for the file.
-   * However this path needs to be absolute. If we want to that path be
-   * relative, we need to indicate relative to what as a second argument.
-   *
-   * Another way is use the 'path' module which is a core module of node.
-   */
-  res.sendFile('./views/index.html', { root: __dirname });
+  // here we going to tell how the view is called and his extension
+  res.render('index');
 });
 
 app.get('/about', (req, res) => {
-  // res.send('<p>About page</p>');
-  res.sendFile('./views/about.html', { root: __dirname });
+  res.render('about');
 });
 
-// redirects
-app.get('/about-us', (req, res) => {
-  res.redirect('/about')
-});
+app.get('/blogs/create', (req, res) => {
+  res.render('create');
+})
 
-// 404 page
-// For redirect to a 404 page we use the method 'use' of express which use a sort of
-// middleware.
-// This work like a switch statement. Express goes from every get request
-// If is not a single match, then this use function is executed
-// fpr that reason this request need to be at the bottom
+/**
+ * 404 page
+ * For redirect to a 404 page we use the method 'use' of express which use a sort of
+ * middleware.
+ * This work like a switch statement. Express goes from every get request
+ * If is not a single match, then this use function is executed
+ * fpr that reason this request need to be at the bottom
+ */
 app.use((req, res) => {
-  res.sendFile('./views/404.html', { root: __dirname });
+  res.status(404).render('404');
 });
