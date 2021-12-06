ab5d8cc8412d18da5eb2a34bbff364a73710bcd5
Juan Sifontez <juansfontz@gmail.com>
2021-12-06 01:39:34 -0400

    Lesson 10: Add route parameters
    Create details view
    Add anchor tag poiting to details view
    Add routing for details blogpost using id param

--- app.js
+++ app.js
@@ -76,6 +76,16 @@ app.post('/blogs', (req, res) => {
     .catch(err => consoe.log(err))
 })
 
+app.get('/blogs/:id', (req, res) => {
+  const id = req.params.id; // we use 'req.params.whateverYouCallItInTheGetReq'
+  // Now we use the 'findById' method of mongoose
+  Blog.findById(id)
+    .then(result => {
+      res.render('details', { blog: result, title: 'Blog details' });
+    })
+    .catch(err => console.log(err))
+});
+
 app.get('/blogs/create', (req, res) => {
   res.render('create', { title: 'Create new Blog' });
 })
